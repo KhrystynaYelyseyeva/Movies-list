397c05689841b26546529358e23f5cdfad9618a6
KhrystynaYelyseyeva <play1409@gmail.com>
2020-04-28 19:57:51 +0300

    1.0

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import MovieItem from "./MovieItem";
+import {moviesData} from "./movieData";
+
+class App extends Component {
+    state = {
+        movies:moviesData,
+        src: 'https://upload.wikimedia.org/wikipedia/ru/thumb/d/de/Форрест_Гамп.jpg/227px-Форрест_Гамп.jpg',
+        title: 'Forrest Gump',
+        vote_average: 9.8,
+        overview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        show: false,
+        like:false,
+    };
+
+    showOverview = () => {
+        this.setState((prevState, props) => {
+                return {show: !prevState.show}
+            });
+    };
+
+    likeMovie=()=>{
+        this.setState((prevState,props)=>{
+           return {like: !prevState.like}
+        });
+    };
+
+    render() {
+        const {src, title, vote_average, overview, show, like} = this.state;
+
+        return (
+            <MovieItem likeMovie={this.likeMovie}
+                       like={like}
+                       showOverview={this.showOverview}
+                       show={show}
+                       overview={overview}
+                       src={src}
+                       title={title}
+                       vote_average={vote_average}/>
+        );
+    }
 }
 
 export default App;

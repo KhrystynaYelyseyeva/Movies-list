72e287875f4f09424d6dae5f7357cf2d214f8d40
KhrystynaYelyseyeva <play1409@gmail.com>
2020-05-02 19:34:47 +0300

    Updates

---
+++ precache-manifest.11b50028f6962dd562d05f4afa4ec249.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efb21505d73827e5d67be1c3fa3cb608",
+    "url": "/Movies-list/index.html"
+  },
+  {
+    "revision": "8acd49fa70bf552367f4",
+    "url": "/Movies-list/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "6ed92b4098b09dbf7ed3",
+    "url": "/Movies-list/static/css/main.0ec2903b.chunk.css"
+  },
+  {
+    "revision": "8acd49fa70bf552367f4",
+    "url": "/Movies-list/static/js/2.f69888ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movies-list/static/js/2.f69888ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ed92b4098b09dbf7ed3",
+    "url": "/Movies-list/static/js/main.071a98f5.chunk.js"
+  },
+  {
+    "revision": "93ec2ead62053e638ad8",
+    "url": "/Movies-list/static/js/runtime-main.959ae66e.js"
+  }
+]);
